120d37398cc9c3cbfd6f71a8cf62fca356c36738
WinnyDrancourt <drancourtwinny@gmail.com>
2025-01-24 09:44:25 +0100

    little adjust of style

--- src/shared/ui/input/input.jsx
+++ src/shared/ui/input/input.jsx
@@ -2,9 +2,9 @@ export default function Input({ type = 'text', onChange, className = '', ...prop
   const textLikeTypes = ['text', 'password', 'email', 'search', 'tel', 'url'];
   const isTextLike = textLikeTypes.includes(type);
 
-  const combinedClasses = `bg-transparent w-full h-full text-primary font-bold py-2 ${
+  const combinedClasses = `bg-transparent w-full h-full text-primary font-bold ps-1 py-2 ${
     isTextLike
-      ? 'border-b-2 border-input focus:outline-none focus:border-b-3 focus:border-primary placeholder-input focus:placeholder-primary/50'
+      ? 'border-input border-b hover:border-b-2 focus:border-b-2 focus:outline-none focus:border-primary placeholder-input focus:placeholder-primary/50'
       : ''
   } ${className}`;
 
