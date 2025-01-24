9375640e7c416c0a83aeb0524e8010e3b371f0bd
WinnyDrancourt <drancourtwinny@gmail.com>
2025-01-24 10:30:19 +0100

    footer style fix width

--- src/shared/layout/footer/footer.jsx
+++ src/shared/layout/footer/footer.jsx
@@ -1,7 +1,8 @@
 import { Link } from 'react-router-dom';
 export default function Footer () {
   return (
-    <footer className="w-full flex justify-around items-center py-4 text-xs border-t border-border/20">
+    <footer className="w-full text-xs border-t border-border/20 py-4 ">
+      <div className="flex justify-between items-center px-10 lg:px-0 lg:w-5/6 mx-auto">
       <p>© 2025 Agent</p>
       <div className="flex flex-col md:flex-row items-center gap-4">
         <Link to="/policy/privacy">
@@ -15,6 +16,7 @@ export default function Footer () {
       {/* Icon de discord */}
       <p className="hidden md:block">Join our community on Discord</p>
       <p className="md:hidden">Discord</p>
+      </div>
     </footer>
   );
 };
