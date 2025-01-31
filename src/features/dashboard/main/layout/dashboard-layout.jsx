1e28715d8afbb7f200ac11fb046979135d8f8beb
ThibaultL24 <thibault.lenormand24@gmail.com>
2025-01-31 20:32:21 +0100

    fix responsive

--- src/features/dashboard/main/layout/dashboard-layout.jsx
+++ src/features/dashboard/main/layout/dashboard-layout.jsx
@@ -5,17 +5,20 @@ export function DashboardLayout() {
   return (
     <div className="p-4 sm:p-6 md:p-8">
       <div className="max-w-7xl mx-auto">
-        {/* Barre XP mobile et tablette */}
-        <div className="lg:hidden mb-12">
-          <XPProgressBar className="h-[2.5rem]" />
+        {/* Barre XP */}
+        <div className="mb-6 md:mb-8 lg:hidden">
+          <XPProgressBar />
         </div>
 
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 md:gap-8 lg:gap-10">
+          {/* Première et deuxième colonnes */}
           <DashboardCards />
           
           {/* Troisième colonne */}
-          <div className="hidden lg:flex flex-col h-[37.5rem] justify-between">
-            <XPProgressBar />
+          <div className="order-last md:col-span-2 lg:col-span-1 flex flex-col h-auto lg:h-[37.5rem] justify-between">
+            <div className="hidden lg:block">
+              <XPProgressBar />
+            </div>
             <ScheduleCards />
           </div>
         </div>
