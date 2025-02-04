f4468b037d9803135109c49354c7d01e5a1f9ccd
WinnyDrancourt <drancourtwinny@gmail.com>
2025-02-04 17:37:14 +0100

    auth config api

--- src/utils/api/auth.api.js
+++ src/utils/api/auth.api.js
@@ -1,22 +1,16 @@
 import Cookies from "js-cookie";
 import { BASE_URL, kyInstance } from "@api/ky-config";
+import { cleanUserData } from "@utils/api/auth.utils";
 
 export async function authSignInUp(object, data) {
   try {
     let response = await kyInstance.post(BASE_URL + object, {
       json: data,
     });
-    // Cookies.set("agent-auth", response.headers.get("Authorization"), {
-    //   secure: true,
-    //   sameSite: "strict",
-    // });
     const userData = await response.json();
-    // const refreshToken = userData.refreshToken;
-    // Cookies.set("agent-refresh", refreshToken, {
-    //   secure: true,
-    //   sameSite: "strict",
-    // });
-    return await userData;
+    if (userData.token) {
+      return { user: cleanUserData(userData.user), token: userData.token };
+    }
   } catch (error) {
     let errorData = await error.responseData;
     throw new Error(JSON.stringify(errorData));
@@ -26,13 +20,11 @@ export async function authSignInUp(object, data) {
 export async function authSignOut() {
   try {
     const accessToken = Cookies.get("agent-auth");
-    const refreshToken = Cookies.get("agent-refresh");
-    if (!accessToken || !refreshToken) {
+    if (!accessToken) {
       throw new Error("Aucun token d'authentification trouvé.");
     }
     let response = await kyInstance.post(`${BASE_URL}signout`, {});
     Cookies.remove("agent-auth");
-    Cookies.remove("agent-refresh");
     return response;
   } catch (error) {
     const errorData = await error.responseData.errors;
