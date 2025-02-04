d9f65a821381f25593b0f89a76a08bda32f702e6
WinnyDrancourt <drancourtwinny@gmail.com>
2025-02-04 21:04:57 +0100

    users auth + edit

--- src/features/users/auth/header-part.jsx
+++ src/features/users/auth/header-part.jsx
@@ -1,33 +1,71 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import Login from "@features/users/auth/login";
+import { useAuth } from "@context/auth.context";
+import { Avatar, AvatarFallback, AvatarImage } from "@shared/ui/avatar";
+import { Button } from "@shared/ui/button";
 import { Bot } from "lucide-react";
 
 export default function HeaderPart() {
+  const { user, isLoading, logout } = useAuth();
   const [isOpen, setIsOpen] = useState(false);
 
-  const handleMouseEnter = () => {
-    setIsOpen(true); // Ouvre le pop-up
-  };
+  const handleMouseEnter = () => setIsOpen(true);
+  const handleMouseLeave = () => setIsOpen(false);
 
-  const handleMouseLeave = () => {
-    setIsOpen(false); // Ferme le pop-up
-  };
+  if (isLoading) return null;
 
   return (
     <div className="relative">
-      <Link
-        className="flex items-center gap-2 opacity-70 hover:opacity-100"
-        onMouseEnter={handleMouseEnter}
-        to="users/login"
-      >
-        <Bot size={32} className="text-primary" />
-        <span className="hidden md:block text-white translate-y-0.5">
-          Sign in
-        </span>
-      </Link>
+      {user ? (
+        <div
+          onMouseEnter={handleMouseEnter}
+          onMouseLeave={handleMouseLeave}
+          className="flex items-center gap-2 opacity-70 hover:opacity-100 p-2 ps-4 rounded-full border border-border"
+        >
+          <p className="text-bold">{user.username.toUpperCase()}</p>
+          <Avatar className="cursor-pointer">
+            <AvatarImage src={user.asset} alt={user.username} />
+            <AvatarFallback className="bg-background">
+              <Bot size={32} className="text-primary" />
+            </AvatarFallback>
+          </Avatar>
 
-      {isOpen && (
+          {isOpen && (
+            <div
+              className="absolute top-10 right-0 w-96 p-5 bg-background border border-border rounded-lg"
+              onMouseEnter={handleMouseEnter}
+              onMouseLeave={handleMouseLeave}
+            >
+              <div className="flex flex-col items-center justify-between gap-2">
+                <p className="">{user.username}</p>
+                <Link to="/users/profile" className="hover:text-primary">
+                  My profile | Edit
+                </Link>
+                <Button
+                  variant="ghost"
+                  className="text-xs text-muted-foreground hover:bg-destructive/70"
+                  onClick={logout}
+                >
+                  Sign out
+                </Button>
+              </div>
+            </div>
+          )}
+        </div>
+      ) : (
+        <Link
+          className="flex items-center gap-2 opacity-70 hover:opacity-100"
+          onMouseEnter={handleMouseEnter}
+          to="users/login"
+        >
+          <span className="hidden md:block text-white translate-y-0.5">
+            Sign in
+          </span>
+          <Bot size={32} className="text-primary" />
+        </Link>
+      )}
+      {!user && isOpen && (
         <div
           className="absolute top-10 right-0 w-96 p-5 bg-background border border-border rounded-lg"
           onMouseEnter={handleMouseEnter}
--- src/features/users/profile.jsx
+++ src/features/users/profile.jsx
@@ -1,47 +1,57 @@
-import { useNavigate } from "react-router-dom";
 import { Button } from "@ui/button";
 import { Input } from "@ui/input";
-import { authSignInUp } from "@api/auth.api";
 import useForm from "@features/users/hook/useForm";
 import { useAuth } from "@context/auth.context";
 
 export default function Login() {
-  const navigate = useNavigate();
-  const { login } = useAuth();
+  const { user } = useAuth();
   const { values, errors, loading, handleChange, handleSubmit } = useForm(
     {
+      username: "",
       email: "",
       password: "",
     },
     "login",
   );
 
-  const handleLogin = async (data) => {
-    try {
-      const payload = {
-        user: {
-          email: data.email,
-          password: data.password,
-        },
-      };
-      const response = await authSignInUp("/api/v1/login", payload);
-      console.log("message", response.message);
-      if (response.token && response.user) {
-        login(response.user, response.token);
-        navigate("/dashboard");
-      }
-    } catch (error) {
-      console.log(error);
-    }
+  const handleEdit = async (data) => {
+    //TODO edit function
+    //   try {
+    //     const payload = {
+    //       user: {
+    //         username: data.username,
+    //         email: data.email,
+    //         password: data.password,
+    //       },
+    //     };
+    //     const response = await authSignInUp("/api/v1/login", payload);
+    //     console.log("message", response.message);
+    //     if (response.token && response.user) {
+    //       login(response.user, response.token);
+    //       navigate("/dashboard");
+    //     }
+    //   } catch (error) {
+    //     console.log(error);
+    //   }
   };
+
   return (
     <form
       onSubmit={(e) => {
         e.preventDefault();
-        handleSubmit(handleLogin);
+        handleSubmit(handleEdit);
       }}
       className="flex flex-col items-center justify-center gap-4 w-full"
     >
+      <Input
+        type="text"
+        name="username"
+        placeholder={user.username}
+        value={values.username}
+        onChange={handleChange}
+      />
+      {errors.username && <p className="text-red-500">{errors.username}</p>}
+
       <Input
         type="text"
         name="email"
@@ -65,7 +75,7 @@ export default function Login() {
         disabled={loading}
         className="w-full text-background"
       >
-        {loading ? "Loading..." : "Login"}
+        {loading ? "Loading..." : "Edit"}
       </Button>
     </form>
   );
