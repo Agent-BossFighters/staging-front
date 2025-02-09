2d825d9d8ef81f8387a6b0cd30b01a6a534ea285
WinnyDrancourt <drancourtwinny@gmail.com>
2025-02-09 19:46:55 +0100

    replace nav with namenu shadcn

--- src/features/users/auth/header-part.jsx
+++ src/features/users/auth/header-part.jsx
@@ -5,81 +5,83 @@ import { useAuth } from "@context/auth.context";
 import { Avatar, AvatarFallback, AvatarImage } from "@shared/ui/avatar";
 import { Button } from "@shared/ui/button";
 import { Bot } from "lucide-react";
+import {
+  NavigationMenu,
+  NavigationMenuList,
+  NavigationMenuItem,
+  NavigationMenuTrigger,
+  NavigationMenuContent,
+  NavigationMenuLink,
+} from "@ui/navigation-menu";
 
 export default function HeaderPart() {
   const { user, isLoading, logout } = useAuth();
-  const [isOpen, setIsOpen] = useState(false);
-
-  const handleMouseEnter = () => setIsOpen(true);
-  const handleMouseLeave = () => setIsOpen(false);
 
   if (isLoading) return null;
 
   return (
-    <div className="relative">
-      {user ? (
-        <div
-          onMouseEnter={handleMouseEnter}
-          onMouseLeave={handleMouseLeave}
-          className="flex items-center gap-2 opacity-70 hover:opacity-100 p-2 ps-4 rounded-full border border-border"
-        >
-          <p className="text-bold">{user.username.toUpperCase()}</p>
-          <Avatar className="cursor-pointer">
-            <AvatarImage src={user.asset} alt={user.username} />
-            <AvatarFallback className="bg-background">
-              <Bot size={32} className="text-primary" />
-            </AvatarFallback>
-          </Avatar>
-
-          {isOpen && (
-            <div
-              className="absolute top-10 right-0 w-96 p-5 bg-background border border-border rounded-lg"
-              onMouseEnter={handleMouseEnter}
-              onMouseLeave={handleMouseLeave}
-            >
-              <div className="flex flex-col items-center justify-between gap-2">
-                <p className="">{user.username}</p>
-                <Link to="/users/profile" className="hover:text-primary">
-                  My profile | Edit
-                </Link>
-                <Button
-                  variant="ghost"
-                  className="text-xs text-muted-foreground hover:bg-destructive/70"
-                  onClick={logout}
-                >
-                  Sign out
-                </Button>
-              </div>
-            </div>
+    <NavigationMenu>
+      <NavigationMenuList>
+        <NavigationMenuItem>
+          {user ? (
+            <>
+              <NavigationMenuTrigger className="h-full flex items-center gap-4 p-2 ps-6 rounded-full border border-border/20 hover:bg-primary hover:text-background">
+                <p className="text-bold">{user.username.toUpperCase()}</p>
+                <Avatar className="cursor-pointer">
+                  <AvatarImage src={user.asset} alt={user.username} />
+                  <AvatarFallback className="bg-background">
+                    <Bot size={32} className="text-primary bg-transparent" />
+                  </AvatarFallback>
+                </Avatar>
+              </NavigationMenuTrigger>
+              <NavigationMenuContent className="w-full p-5 bg-background border border-border rounded-lg text-foreground">
+                <div className="flex flex-col items-center justify-between gap-2">
+                  <p>{user.username}</p>
+                  <NavigationMenuLink asChild>
+                    <Link to="/users/profile" className="hover:text-primary">
+                      My profile | Edit
+                    </Link>
+                  </NavigationMenuLink>
+                  <Button
+                    variant="ghost"
+                    className="text-xs text-muted-foreground hover:bg-destructive/70"
+                    onClick={logout}
+                  >
+                    Sign out
+                  </Button>
+                </div>
+              </NavigationMenuContent>
+            </>
+          ) : (
+            <NavigationMenuLink asChild>
+              <Link
+                className="flex items-center gap-2 opacity-70 hover:opacity-100"
+                to="users/login"
+              >
+                <span className="hidden md:block text-white translate-y-0.5">
+                  Sign in
+                </span>
+                <Bot size={32} className="text-primary" />
+              </Link>
+            </NavigationMenuLink>
           )}
-        </div>
-      ) : (
-        <Link
-          className="flex items-center gap-2 opacity-70 hover:opacity-100"
-          onMouseEnter={handleMouseEnter}
-          to="users/login"
-        >
-          <span className="hidden md:block text-white translate-y-0.5">
-            Sign in
-          </span>
-          <Bot size={32} className="text-primary" />
-        </Link>
-      )}
-      {!user && isOpen && (
-        <div
-          className="absolute top-10 right-0 w-96 p-5 bg-background border border-border rounded-lg"
-          onMouseEnter={handleMouseEnter}
-          onMouseLeave={handleMouseLeave}
-        >
-          <Login />
-          <div className="mt-5 text-end text-xs flex justify-end">
-            <p>Don&#39;t have an account?&nbsp;</p>
-            <Link to="/users/register" className="text-primary">
-              Sign up
-            </Link>
-          </div>
-        </div>
-      )}
-    </div>
+        </NavigationMenuItem>
+        {!user && (
+          <NavigationMenuItem>
+            <NavigationMenuContent className="w-96 p-5 bg-background border border-border rounded-lg">
+              <Login />
+              <div className="mt-5 text-end text-xs flex justify-end">
+                <p>Don&#39;t have an account?&nbsp;</p>
+                <NavigationMenuLink asChild>
+                  <Link to="/users/register" className="text-primary">
+                    Sign up
+                  </Link>
+                </NavigationMenuLink>
+              </div>
+            </NavigationMenuContent>
+          </NavigationMenuItem>
+        )}
+      </NavigationMenuList>
+    </NavigationMenu>
   );
 }
