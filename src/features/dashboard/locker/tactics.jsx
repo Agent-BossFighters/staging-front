9663436918d1213c5e5a6dc7fb2021bf20a52602
ThibaultL24 <thibault.lenormand24@gmail.com>
2025-02-17 15:12:49 +0100

    fix size locker with omiage

--- src/features/dashboard/locker/tactics.jsx
+++ src/features/dashboard/locker/tactics.jsx
@@ -21,19 +21,19 @@ export default function Tatics() {
 
   return (
     <div className="flex flex-col gap-3">
-      <h2 className="text-3xl font-extrabold py-2 flex gap-3 items-center">
+      <h2 className="text-2xl font-extrabold py-2 flex gap-3 items-center">
         <img src={Contract} alt="contract" className="w-10 h-10" />
         MY TACTICS
       </h2>
       <div className="flex flex-col lg:flex-row gap-5 lg:gap-9">
         <div className="flex flex-col">
-          <h3 className="text-xl font-bold whitespace-nowrap">
+          <h3 className="text-l font-bold whitespace-nowrap">
             MAX ITEM RARITY TO SHOW
           </h3>
           <SelectSlot onSelectRarity={(value) => setMaxRarity(value)} />
         </div>
         <div className="flex flex-col">
-          <h3 className="text-xl font-bold whitespace-nowrap">
+          <h3 className="text-l font-bold whitespace-nowrap">
             MY FAVORITE FLEX PACK
           </h3>
           <Select value={selectedValue1} onValueChange={setSelectedValue1}>
@@ -53,7 +53,7 @@ export default function Tatics() {
           </Select>
         </div>
         <div className="flex flex-col">
-          <h3 className="text-xl font-bold whitespace-nowrap">
+          <h3 className="text-l font-bold whitespace-nowrap">
             BADGE SLOT(S) UNLOCKED
           </h3>
           <Select value={selectedValue} onValueChange={setSelectedValue}>
