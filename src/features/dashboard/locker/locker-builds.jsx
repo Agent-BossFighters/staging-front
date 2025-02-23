91cedc84936df522d9ddb0a7174c098f48b72fb7
ThibaultL24 <thibault.lenormand24@gmail.com>
2025-02-24 00:31:32 +0100

    fix table recharge in locker

--- src/features/dashboard/locker/locker-builds.jsx
+++ src/features/dashboard/locker/locker-builds.jsx
@@ -99,123 +99,125 @@ export default function Lockerbuilds() {
   }
 
   return (
-    <div className="lg:h-1/2">
+    <div className="flex flex-col h-[400px]">
       <h2 className="text-3xl font-extrabold py-2 flex gap-3 items-center">
         <img src={GreenFighter} alt="Build" className="w-10 h-10" />
         BUILD(S)
       </h2>
-      <Table className="">
-        <TableCaption>Desc ?</TableCaption>
-        <TableHeader>
-          <TableRow className="bg-muted-foreground/30">
-            <TableHead>BUILD NAME</TableHead>
-            <TableHead>BONUS MULTIPLIER</TableHead>
-            <TableHead>PERKS MULTIPLIER</TableHead>
-            <TableHead>ACTION(S)</TableHead>
-          </TableRow>
-        </TableHeader>
-        <TableBody className="overflow-y-auto">
-          {builds.length > 0 ? (
-            builds.map((build, index) => {
-              const isEditing = build.id === editingBuildId;
+      <div className="flex-grow overflow-auto">
+        <Table className="">
+          <TableCaption>Desc ?</TableCaption>
+          <TableHeader>
+            <TableRow className="bg-muted-foreground/30">
+              <TableHead>BUILD NAME</TableHead>
+              <TableHead>BONUS MULTIPLIER</TableHead>
+              <TableHead>PERKS MULTIPLIER</TableHead>
+              <TableHead>ACTION(S)</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody className="overflow-y-auto">
+            {builds.length > 0 ? (
+              builds.map((build, index) => {
+                const isEditing = build.id === editingBuildId;
 
-              return (
-                <TableRow key={index} className="">
-                  <TableCell>
-                    {isEditing ? (
-                      <Input
-                        type="text"
-                        value={editedName}
-                        onChange={(e) => setEditedName(e.target.value)}
+                return (
+                  <TableRow key={index} className="">
+                    <TableCell>
+                      {isEditing ? (
+                        <Input
+                          type="text"
+                          value={editedName}
+                          onChange={(e) => setEditedName(e.target.value)}
+                        />
+                      ) : (
+                        build.buildName
+                      )}
+                    </TableCell>
+                    <TableCell>
+                      {isEditing ? (
+                        <NumericInput
+                          placeholder="Bonus Multiplier"
+                          value={editedBonus}
+                          onChange={setEditedBonus}
+                          className="w-1/2"
+                        />
+                      ) : (
+                        build.bonusMultiplier
+                      )}
+                    </TableCell>
+                    <TableCell>
+                      {isEditing ? (
+                        <NumericInput
+                          placeholder="Perks Multiplier"
+                          value={editedPerks}
+                          onChange={setEditedPerks}
+                          className="w-1/2"
+                        />
+                      ) : (
+                        build.perksMultiplier
+                      )}
+                    </TableCell>
+                    <TableCell className="flex gap-2 items-center">
+                      <ActionsTable
+                        data={build}
+                        onEdit={handleEdit}
+                        onDelete={() => handleDelete(build.id)}
+                        onSave={handleSave}
+                        onCancel={handleCancel}
+                        isEditing={isEditing}
                       />
-                    ) : (
-                      build.buildName
-                    )}
-                  </TableCell>
-                  <TableCell>
-                    {isEditing ? (
-                      <NumericInput
-                        placeholder="Bonus Multiplier"
-                        value={editedBonus}
-                        onChange={setEditedBonus}
-                        className="w-1/2"
-                      />
-                    ) : (
-                      build.bonusMultiplier
-                    )}
-                  </TableCell>
-                  <TableCell>
-                    {isEditing ? (
-                      <NumericInput
-                        placeholder="Perks Multiplier"
-                        value={editedPerks}
-                        onChange={setEditedPerks}
-                        className="w-1/2"
-                      />
-                    ) : (
-                      build.perksMultiplier
-                    )}
-                  </TableCell>
-                  <TableCell className="flex gap-2 items-center">
-                    <ActionsTable
-                      data={build}
-                      onEdit={handleEdit}
-                      onDelete={() => handleDelete(build.id)}
-                      onSave={handleSave}
-                      onCancel={handleCancel}
-                      isEditing={isEditing}
-                    />
-                  </TableCell>
-                </TableRow>
-              );
-            })
-          ) : (
+                    </TableCell>
+                  </TableRow>
+                );
+              })
+            ) : (
+              <TableRow>
+                <TableCell colSpan={5} className="text-center">
+                  No Builds found
+                </TableCell>
+              </TableRow>
+            )}
+          </TableBody>
+          <TableFooter className="bg-transparent">
             <TableRow>
-              <TableCell colSpan={5} className="text-center">
-                No Builds found
+              <TableCell>
+                <Input
+                  type="text"
+                  placeholder="Build Name"
+                  inputMode="numeric"
+                  value={buildName}
+                  onChange={(e) => setBuildName(e.target.value)}
+                />
+              </TableCell>
+              <TableCell>
+                <NumericInput
+                  placeholder="Bonus Multiplier"
+                  value={bonus}
+                  onChange={setBonus}
+                  className="w-1/2"
+                />
+              </TableCell>
+              <TableCell>
+                <NumericInput
+                  placeholder="Perks Multiplier"
+                  value={perks}
+                  onChange={setPerks}
+                  className="w-1/2"
+                />
+              </TableCell>
+              <TableCell className="flex items-center">
+                <Button
+                  variant="transparent"
+                  onClick={handleSubmit}
+                  className="p-0 hover:text-primary hover:scale-150"
+                >
+                  <Plus />
+                </Button>
               </TableCell>
             </TableRow>
-          )}
-        </TableBody>
-        <TableFooter className="bg-transparent">
-          <TableRow>
-            <TableCell>
-              <Input
-                type="text"
-                placeholder="Build Name"
-                inputMode="numeric"
-                value={buildName}
-                onChange={(e) => setBuildName(e.target.value)}
-              />
-            </TableCell>
-            <TableCell>
-              <NumericInput
-                placeholder="Bonus Multiplier"
-                value={bonus}
-                onChange={setBonus}
-                className="w-1/2"
-              />
-            </TableCell>
-            <TableCell>
-              <NumericInput
-                placeholder="Perks Multiplier"
-                value={perks}
-                onChange={setPerks}
-                className="w-1/2"
-              />
-            </TableCell>
-            <TableCell className="flex items-center">
-              <Button
-                variant="transparent"
-                onClick={handleSubmit}
-                className="p-0 hover:text-primary hover:scale-150"
-              >
-                <Plus />
-              </Button>
-            </TableCell>
-          </TableRow>
-        </TableFooter>
-      </Table>
+          </TableFooter>
+        </Table>
+      </div>
     </div>
   );
 }
