cd2a05b21dee555df1da31b8ce2d584b9880d235
ThibaultL24 <thibault.lenormand24@gmail.com>
2025-02-25 10:37:06 +0100

    fix titles in monthly

--- src/features/dashboard/monthly/monthly-matches.jsx
+++ src/features/dashboard/monthly/monthly-matches.jsx
@@ -50,13 +50,13 @@ export default function MonthlyMatches({
           <TableHeader className="sticky top-0 z-10">
             <TableRow className="bg-muted-foreground/30">
               <TableHead className="text-left">DATE</TableHead>
-              <TableHead className="text-center">MATCHES PLAYED</TableHead>
-              <TableHead className="text-center">IG TIME (Min)</TableHead>
-              <TableHead className="text-center">ENERGY USED</TableHead>
+              <TableHead className="text-center">MATCHES<br /> PLAYED</TableHead>
+              <TableHead className="text-center">IG TIME<br /> (Min)</TableHead>
+              <TableHead className="text-center">ENERGY<br /> USED</TableHead>
               <TableHead className="text-center text-destructive">
-                ENERGY COST
+                ENERGY<br /> COST
               </TableHead>
-              <TableHead className="text-center">TOTAL $BFT</TableHead>
+              <TableHead className="text-center">TOTAL<br /> $BFT</TableHead>
               <TableHead className="text-center text-accent">BFT ($)</TableHead>
               <TableHead className="text-center">FLEX</TableHead>
               <TableHead className="text-center text-accent">
