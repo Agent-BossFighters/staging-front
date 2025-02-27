9305f5ad55c78f37e35815e5a744e4c5feae5419
WinnyDrancourt <drancourtwinny@gmail.com>
2025-02-27 17:13:53 +0100

    Fix size img Economiemap

--- src/pages/static/economy.page.jsx
+++ src/pages/static/economy.page.jsx
@@ -2,12 +2,12 @@ import { Economy } from "@img/index";
 
 export default function EconomyPage() {
   return (
-    <div className="w-5/6 mx-auto h-full">
-      <h1 className="text-primary text-6xl font-bold ps-20 pt-9">
+    <div className="w-5/6 mx-auto">
+      <h1 className="text-primary text-6xl font-bold ps-20 py-9">
         ECONOMY MAP
       </h1>
       <div className="flex items-center justify-center h-full pb-9">
-        <img src={Economy} alt="Economy" className="h-full object-fit" />
+        <img src={Economy} alt="Economy" className="h-full object-contain" />
       </div>
     </div>
   );
