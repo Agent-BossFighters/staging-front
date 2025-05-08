1860388801c7af7a106d9ee63b80cb068b235d5c
AlexandreTedesco <alexandre.tedesco75@gmail.com>
2025-05-09 01:34:16 +0200

    comment "player map" but lib is in local

--- src/pages/dashboard/playerMap/playerMapView.jsx
+++ src/pages/dashboard/playerMap/playerMapView.jsx
@@ -2,7 +2,7 @@ import {
   PlayerMapHome,
   RegistrationForm,
   GraphComponent,
-  PlayerMapGraph,
+  // PlayerMapGraph,
 } from "player-map";
 import { usePrivy } from "@privy-io/react-auth";
 import { useState, useEffect } from "react";
@@ -87,7 +87,7 @@ export default function PlayerMapView() {
           usePublicClient,
         }}
       />
-      <PlayerMapGraph {...registrationProps} />
+      {/* <PlayerMapGraph {...registrationProps} /> */}
     </div>
   );
 }
